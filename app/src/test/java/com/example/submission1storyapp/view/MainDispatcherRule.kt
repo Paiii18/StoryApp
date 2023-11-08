312a7598959f996f3ecf23b56d35e309815aa6ec
Sahril <103849124+CallMeSahril@users.noreply.github.com>
2023-11-08 20:34:34 +0700

    BUg FIx

--- app/src/test/java/com/example/submission1storyapp/view/MainDispatcherRule.kt
+++ app/src/test/java/com/example/submission1storyapp/view/MainDispatcherRule.kt
@@ -2,6 +2,10 @@ package com.example.submission1storyapp.view
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
